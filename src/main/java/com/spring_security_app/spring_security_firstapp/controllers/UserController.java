dc4076057e642945a8ff424dfc6f5e4ebd2064ed
outr1ght <ppraktika@bk.ru>
2023-03-16 21:35:01 +0300

    some fixes

--- src/main/java/com/spring_security_app/spring_security_firstapp/controllers/UserController.java
+++ src/main/java/com/spring_security_app/spring_security_firstapp/controllers/UserController.java
@@ -2,7 +2,6 @@ package com.spring_security_app.spring_security_firstapp.controllers;
 
 
 import com.spring_security_app.spring_security_firstapp.entities.User;
-import com.spring_security_app.spring_security_firstapp.repositories.UserRepository;
 import com.spring_security_app.spring_security_firstapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,18 +9,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
-import java.util.Optional;
 
 @Controller
 public class UserController {
 
     private final UserService userService;
-    private final UserRepository userRepository;
 
     @Autowired
-    public UserController(UserService userService, UserRepository userRepository) {
+    public UserController(UserService userService) {
         this.userService = userService;
-        this.userRepository = userRepository;
     }
 
     @GetMapping("/user")
